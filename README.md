c31c48616d582b9d039def0578c8d6ea247aeb78
prio <prioteasa.andrei77@gmail.com>
2025-02-15 16:48:13 +0100

    reading and example of pyMC

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is the final project for the Computational Cognitive Science class, WS24/25
 - smaller reaction time doesn't necessary mean easier decision (e.g. impulsiveness)
 - same choice modeling as in class, only reaction time modelling differs.
 - only two modalities used
+- think about a probability weighting function to model subjective probabilities for impulsivness and risk aversion (use a measure of objective value for each value probabilistic vs immediate and from that abstract personal subjectivity). put this into the choice not into the response times (see course 4).
 
 # Setup
 
@@ -17,7 +18,16 @@ conda activate pd-ws24
 
 # Good practice
 
+- try to use pyMC(https://www.pymc.io/welcome.html) for the modeling part
 
 # TODOS
 
 - [ ] use MTF as an approach for combining multimodal data
+- [ ] implement hierachical modeling, perhaps with PyMC for sampling the join posterior
+- [ ] implement K-fold validation
+    - [ ] implement in-sample and out of sample error and do box plots with whiskers (i.e. mean the predicitons since we will have a probailistic model and for out of sample do the same with CV)
+    - [ ] tune hyperparams on the mean validation error from all folds
+- [ ] OPTIONAL: active learning
+- [ ] model comparison with AIC and BIC (minimize to get the best)
+- [ ] research meaningful statistical tests that can be used to asses the influence of certain parameters of our models
+- [ ] take condition into consideration for the sign of certain parameters
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,60 @@
 import pandas as pd
 from typing import List
+import pymc as pm
+import numpy as np
+import pandas as pd
+import scipy.io
+import glob
+import os
 
 
 def read_dd_data(path: str) -> pd.DataFrame:
     """
     Read data from the given path and return a DataFrame.
-    Performs normalization and type conversion.
-    """
-    pass
 
+    action (1=certain, 2=uncertain, 0=missing)
+    condition (1=reward,2=loss)
+
+    return: pd.DataFrame with columns: participant, choice, condition, event_prob, odds, rt, rcert
+    """
+    data_files = glob.glob(path + "/*.mat")
+    all_participants = []
+
+    for file in data_files:
+        mat_data = scipy.io.loadmat(file)
+        
+        # Extract training data
+        data_train = pd.DataFrame(mat_data['data_train'], columns=[col[0] for col in mat_data['data_labels'].flatten()])
+        
+        # rename columns
+        data_train = data_train.rename(columns={'certOutcome': 'rcert',
+                                                'uncOutcome': 'runcert',
+                                                'action (1=certain, 2=uncertain, 0=missing)': 'choice',
+                                                'p_cert': 'experimental_condition', ## TODO ???
+                                                'outcome prob.': 'event_prob',
+                                                'condition (1=reward,2=loss)': 'condition',
+                                                'RT': 'rt',
+                                                'odds': 'odds'})
+
+
+        # Add participant identifier
+        data_train['participant'] = os.path.basename(file).replace('.mat', '')
+        
+        all_participants.append(data_train)
+
+    # Combine all participants into a single DataFrame
+    df = pd.concat(all_participants, ignore_index=True)
+
+    # Remove trials where choice is missing (0)
+    df = df[df['choice'] != 0]
+
+    # drop experimental_condition column
+    df = df.drop(columns=['experimental_condition'])
+
+    # normalize probabilities to be between 0 and 1
+    df['event_prob'] = df['event_prob'] / 100
+
+    # make choice 0 be certain and 1 be uncertain
+    df['choice'] = df['choice'] - 1
+
+    return df
